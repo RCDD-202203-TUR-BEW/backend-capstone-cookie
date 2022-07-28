668cfc32609b7f12e5d782998b73e32ae7694124
Emine Çığ <58005344+eminecg@users.noreply.github.com>
2022-07-28 12:19:06 +0300

    fix user and location schema

--- src/models/location.js
+++ src/models/location.js
@@ -1,29 +1,33 @@
-const mongoose= require("mongoose");
-const LocationSchema=mongoose.schemma({
-    city:{
-        type: String,
-        requied: true,
-              },
-    district:{
-        type: String,
-        requied: true,
-             },
-    street:{
-        type: String,
-        requied: true,
-             },
-    quarter:{
-        type: String,
-            },
-    block_num:{
-        type: Number,
-             },
-    flat_num:{
-        type: String,                        
-        requied: true,
-              },
-    location_description:{
-        type: String,
-        requied: true,
-              },         
-})
+const mongoose = require('mongoose');
+
+const LocationSchema = mongoose.schemma({
+  city: {
+    type: String,
+    requied: true,
+  },
+  district: {
+    type: String,
+    requied: true,
+  },
+  street: {
+    type: String,
+    requied: true,
+  },
+  quarter: {
+    type: String,
+  },
+  block_num: {
+    type: Number,
+  },
+  flat_num: {
+    type: String,
+    requied: true,
+  },
+  location_description: {
+    type: String,
+    requied: true,
+  },
+});
+
+const modelName = process.env.LOCATION_MODEL_NAME;
+module.exports = mongoose.model(modelName, LocationSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -1,95 +1,98 @@
-const mongoose = require("mongoose");
-const location=require("./location");
-const UserSchema = mongoose.Schema({
-      id :{type: Schema.Types.ObjectId,},
-      role:{
-            type:string,
-            enum:["chef","customer","admin"]
-      },
+const mongoose = require('mongoose');
+const location = require('./location');
 
-      locations: {
-            ref: 'Location',
-            type: [mongoose.Schema.Types.ObjectId],        
-            default: [],
-          },
-     
-   
-      first_name:{
-        type: String,
-        unique: false,
-        requied: true,
-              },
-   
-      last_name:{
-        type: String,
-        unique: false,
-        requied: true,
-             },
-   
-             user_name: {
-                  type: String,
-                  match: [
-                    // eslint-disable-next-line node/no-unsupported-features/es-syntax
-                    /^(?=.{2,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-                    `invalid username`,
-                  ],
-                  required: true,
-                  unique: true,
-                  lowercase: true,
-                },
-   
-                email: {
-                  type: String,
-                  match: [
-                    /^[a-zA-Z0-9\-_.]+@[a-z]+\.([a-z]{2,3})+(\.[a-z]{2,3})?$/,
-                    `invalid email`,
-                  ],
-                  required: true,
-                  unique: true,
-                  lowercase: true,
-                },
-   
-   email_verified:{ 
-        type:Boolean,
-        requied:true,
-                  },
-   
-   
-   password: {
-        type: String,
-        requied: true,
-             },
-   
-   
-   phone:{
-         type:Number,
-         requied:true,
-         unique:true,
-         },
-   
-   
-   avatar:{
-      type:string,
-   },
-   
-   
-   birthday:{
-    type:Date,
-    requied:true,
-   },
-   
-   
-   created_at:{
-    type: Date,
-    default: Date.now,
-   },
-},
-   {timestamps: true},
-   { toJSON: { virtuals: true } },
-    { toObject: { virtuals: true } }
+const UserSchema = mongoose.Schema(
+  {
+    role: {
+      type: String,
+      enum: ['chef', 'customer', 'admin'],
+    },
 
+    locations: [location],
+
+    first_name: {
+      type: String,
+      unique: false,
+      requied: true,
+    },
+
+    last_name: {
+      type: String,
+      unique: false,
+      requied: true,
+    },
+
+    user_name: {
+      type: String,
+      match: [
+        // eslint-disable-next-line node/no-unsupported-features/es-syntax
+        /^(?=.{2,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+        `invalid username`,
+      ],
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    email: {
+      type: String,
+      match: [
+        /^[a-zA-Z0-9\-_.]+@[a-z]+\.([a-z]{2,3})+(\.[a-z]{2,3})?$/,
+        `invalid email`,
+      ],
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    email_verified: {
+      type: Boolean,
+      requied: true,
+    },
+
+    password_hash: {
+      type: String,
+      required: false,
+    },
+
+    phone: {
+      type: Number,
+      requied: true,
+      unique: true,
+    },
+
+    avatar: {
+      type: String,
+    },
+
+    birthday: {
+      type: Date,
+      requied: true,
+    },
+
+    gender: {
+      type: String,
+      required: true,
+      enum: ['female', 'male', 'non-binary defined'],
+    },
+    provider: {
+      type: String,
+      enum: ['email', 'google', 'twitter'],
+      default: 'email',
+      required: true,
+    },
+    providerId: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+  { toJSON: { virtuals: true } },
+  { toObject: { virtuals: true } }
 );
-userSchema.virtual('full_name').get(function () {
-      return `${this.first_name} ${this.last_name}`;
-  });
-exports.module = User = mongoose.model("User", UserSchema);
+
+UserSchema.virtual('full_name').get(function () {
+  return `${this.first_name} ${this.last_name}`;
+});
+
+const modelName = process.env.USER_MODEL_NAME;
+module.exports = mongoose.model(modelName, UserSchema);
