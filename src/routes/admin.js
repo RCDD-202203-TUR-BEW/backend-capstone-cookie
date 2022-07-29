cb9a54824cbbab3e137798a6ce8640a245a82f02
Huzeyfe <huzeyfe.abdullahoglu@bilgiedu.net>
2022-07-29 16:34:21 +0300

    refactor: edit upon requested

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const adminController = require('../controllers/admin');
 
-router.get('/admin/fetchUser', adminController);
-router.delete('/admin/delete', adminController);
+router.get('/admin/fetchUser', adminController); // fetch spesific user
+router.get('/admin/fetchAllCustomer', adminController); // fetch all customer
+router.get('/admin/fetchAllChef', adminController); // fetch all chefs
 
+router.get('/admin/fetchAll', adminController); // fetch all users
+router.delete('/admin/delete', adminController); // delete user
 module.exports = router;
