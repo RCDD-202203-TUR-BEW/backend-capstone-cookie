97c24dca138e41df2817800c4f56727f9c01d56b
Huzeyfe <huzeyfe.abdullahoglu@bilgiedu.net>
2022-07-29 16:40:39 +0300

    refactor: edit upon requested

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const connectToMongo = require('./db/connection');
 
-//  const router = require('./routes');
-const customerRoutes = require("./routes/customer");
-const chefRoutes = require("./routes/chef");
+ const router = require('./routes');
+// const customerRoutes = require("./routes/customer");
+// const chefRoutes = require("./routes/chef");
 
 const app = express();
 const port = process.env.NODE_LOCAL_PORT;
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use(connectToMongo);
 
 //  app.use('/', router);
-app.use("/api/customer", customerRoutes);
-app.use("/api/chef", chefRoutes);
-
+// app.use("/api/customer", customerRoutes);
+// app.use("/api/chef", chefRoutes);
+app.use('/api', router);
 app.listen(port, () => {
   //    console.log(`Server listening on port ${port}`);
   connectToMongo();
