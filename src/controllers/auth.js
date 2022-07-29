12799079b0a6f36e1a61a5b9e2552d21eb304d9d
Hala Alkhellow <halakhellow1996@gmail.com>
2022-07-29 03:08:06 +0300

    add signup controller

---
+++ src/controllers/auth.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcrypt');
+const Users = require('../models/user');
+
+const authControllers = {};
+
+authControllers.signup = async (req, res) => {
+  const {
+    firstname,
+    lastname,
+    username,
+    email,
+    password,
+    confirmPassword,
+    phone,
+    birthday,
+    gender,
+    acceptTos,
+  } = req.body;
+
+  const existedUser = await Users.findOne({ $or: [{ username }, { email }] });
+  if (existedUser) {
+    return res.status(400).json({
+      error: `${
+        username ? `username: ${username}` : `email: ${email}`
+      } already existed!`,
+    });
+  }
+  if (password !== confirmPassword) {
+    return res.status(400).json({ error: "Passwords don't match" });
+  }
+  if (!acceptTos) {
+    return res
+      .status(400)
+      .json({ error: 'You need to accept terms of service' });
+  }
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  await Users.create({
+    firstname,
+    lastname,
+    username,
+    email,
+    password_hash: hashedPassword,
+    phone,
+    birthday,
+    gender,
+  });
+  return res.redirect('/');
+};
+
+module.exports = authControllers;
