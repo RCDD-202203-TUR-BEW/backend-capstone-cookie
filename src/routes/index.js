04de7e03aabc9e6f63454a1fc80414abe85891b6
Huzeyfe <huzeyfe.abdullahoglu@bilgiedu.net>
2022-07-30 16:25:51 +0300

    add the root routes in specific file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const customerRoutes = require('./customer')
-const chefRoutes = require('./chef')
-const adminRoutes = require('./admin')
+const customerRoutes = require('./customer');
+const chefRoutes = require('./chef');
+const adminRoutes = require('./admin');
 
-router.get('/customer',customerRoutes)   //    /api/customer
-router.get('/chef',chefRoutes)   //    /api/chef
-router.get('/admin',adminRoutes)   //    /api/admin
+router.get('/customer', customerRoutes); //    /api/customer
+router.get('/chef', chefRoutes); //    /api/chef
+router.get('/admin', adminRoutes); //    /api/admin
 
 module.exports = router;
