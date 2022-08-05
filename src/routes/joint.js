bc59112492426579379b4ccffb179b33a5112a51
Huzeyfe <huzeyfe.abdullahoglu@bilgiedu.net>
2022-08-05 21:28:01 +0300

    refactor: the auth&orders routes

--- src/routes/joint.js
+++ src/routes/joint.js
@@ -2,21 +2,14 @@ const express = require('express');
 const router = express.Router();
 const chefController = require('../controllers/chef');
 const customerController = require('../controllers/customer');
+const authControllers = require('../controllers/auth');
+const isAuthenticated = require('../middleware/isAuthenticated');
 
+router.post('/auth/signup', authControllers.signup);
+router.post('/auth/login', authControllers.signin);
+router.get('/auth/signout', isAuthenticated, authControllers.signout);
 
-/**
- * router.use((req, res, next) => {
-  here we can check if the user is chef or customer then redirect to the correct page
-  here we can check if the user is logged in or not and redirect to the correct page
+router.get('/orders', isAuthenticated, customerController); //all orders for customers
+router.get('/orders', isAuthenticated, chefController); // all orders for chefs
 
-  next()
-})
- */
-router.post('/signup', customerController);
-router.post('/login', customerController);
-router.get('/orders', customerController);
-
-router.post('/signup', chefController);
-router.post('/login', chefController);
-router.get('/orders', chefController); // all orders
 module.exports = router;
