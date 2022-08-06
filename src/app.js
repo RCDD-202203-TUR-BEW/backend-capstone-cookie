a3a8727e1a86625ba5036b640dfd441d477ac475
Hala Alkhellow <halakhellow1996@gmail.com>
2022-08-07 02:41:47 +0300

    verify the jwt in app.js using express-jwt package and handle encrypting and decrypting the cookie using the suitable packages

--- src/app.js
+++ src/app.js
@@ -1,24 +1,42 @@
 const express = require('express');
 require('dotenv').config();
+const { expressjwt: jwt } = require('express-jwt');
+const cookieParser = require('cookie-parser');
+const { encryptCookieNodeMiddleware } = require('encrypt-cookie');
+
+const { UnauthorizedErrorHandler } = require('./middleware/errorHandling');
+
 const connectToMongo = require('./db/connection');
 
-const router = require('./routes');
+const apiRoutes = require('./routes');
 const orderRoutes = require('./routes/order');
-
 
 const app = express();
 const port = process.env.NODE_LOCAL_PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(connectToMongo);
 
-app.use('/api', router);
+app.use(cookieParser(process.env.SECRET_KEY));
+app.use(encryptCookieNodeMiddleware(process.env.SECRET_KEY));
+
+app.use(
+  '/api',
+  jwt({
+    secret: process.env.SECRET_KEY,
+    algorithms: ['HS256'],
+    requestProperty: 'user',
+    getToken: (req) => req.signedCookies.token ?? req.cookies.token,
+  }).unless({ path: ['/api/auth/signup', '/api/auth/signin'] })
+);
+
+app.use('/api', apiRoutes);
 app.use(orderRoutes);
 
+app.use(UnauthorizedErrorHandler);
 
 app.listen(port, () => {
-  //    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
   connectToMongo();
 });
 
