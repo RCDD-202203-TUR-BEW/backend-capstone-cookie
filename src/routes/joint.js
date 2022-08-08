55ac8ebc68e733917b81622f78daf79b1abe062a
Kishi <66186149+Kishimoto96@users.noreply.github.com>
2022-08-08 16:08:46 +0300

    eslint fix

--- src/routes/joint.js
+++ src/routes/joint.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const chefController = require('../controllers/chef');
 const customerController = require('../controllers/customer');
@@ -9,7 +10,7 @@ router.post('/auth/signup', authControllers.signup);
 router.post('/auth/login', authControllers.signin);
 router.get('/auth/signout', isAuthenticated, authControllers.signout);
 
-router.get('/orders', isAuthenticated, customerController); //all orders for customers
+router.get('/orders', isAuthenticated, customerController); // all orders for customers
 router.get('/orders', isAuthenticated, chefController); // all orders for chefs
 
 module.exports = router;
