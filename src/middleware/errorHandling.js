e60424d4dcc3c61f5fdaf838cf8784891060d231
Hala Alkhellow <halakhellow1996@gmail.com>
2022-08-08 17:01:52 +0300

    refactor: edit the message shown to the users when there's an unauthorized error

--- src/middleware/errorHandling.js
+++ src/middleware/errorHandling.js
@@ -1,6 +1,6 @@
 const UnauthorizedErrorHandler = (err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
-    res.status(401).send('invalid token...');
+    res.status(401).send("You don't have authorization to view this page");
   } else {
     next(err);
   }
