77e6065dfee866614c7fb1d0654d4e50bd020dcc
Hamza Aymen <hamza.ayment@gmail.com>
2022-08-10 02:05:35 +0300

    .

--- src/app.js
+++ src/app.js
@@ -1,20 +1,59 @@
 const express = require('express');
 require('dotenv').config();
+const { expressjwt: jwt } = require('express-jwt');
+const cookieParser = require('cookie-parser');
+const { encryptCookieNodeMiddleware } = require('encrypt-cookie');
+
+const { UnauthorizedErrorHandler } = require('./middleware/errorHandling');
+
 const connectToMongo = require('./db/connection');
 
-const router = require('./routes');
+const apiRoutes = require('./routes');
+const orderRoutes = require('./routes/order');
+
+const customerRoutes = require('./routes/customer');
+const chefRoutes = require('./routes/chef');
 
 const app = express();
 const port = process.env.NODE_LOCAL_PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(connectToMongo);
 
-app.use('/api', router);
+app.use(cookieParser(process.env.SECRET_KEY));
+app.use(encryptCookieNodeMiddleware(process.env.SECRET_KEY));
+
+const path = [
+  '/api/auth/signup',
+  '/api/auth/signin',
+  '/api/chefs',
+  '/api/chefs/:username',
+  '/api/dishes',
+  '/api/dishes/:dishId',
+  '/api/dishes/filter',
+];
+
+app.use(
+  '/api',
+  jwt({
+    secret: process.env.SECRET_KEY,
+    algorithms: ['HS256'],
+    requestProperty: 'user',
+    getToken: (req) => req.signedCookies.token ?? req.cookies.token,
+  }).unless({
+    path,
+  })
+);
+
+app.use('/api', apiRoutes);
+app.use('/api/customer', customerRoutes);
+app.use('/api/chef', chefRoutes);
+app.use(orderRoutes);
+
+app.use(UnauthorizedErrorHandler);
 
 app.listen(port, () => {
-  //    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
   connectToMongo();
 });
 
---
+++ src/routes/customer.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+const chefController = require('../controllers/chef');
+
+const customerController = require('../controllers/customer');
+
+router.get('/', customerController);
+router.get('/profile', customerController); // profile
+
+router.get('/rate', customerController); // rated dishes
+router.get('/rate/:id', customerController); // rated a single dish
+router.put('/profile', customerController); // update profile - and every thing else inside the profile details
+
+router.put('/orders', customerController); // update orders
+
+router.delete('/deleteaddress', customerController); // delete address
+
+router.delete('/deleteAccount', customerController); // delete account
+router.get('/chefs', chefController); // all chefs"
+router.get('/chefs/:username', chefController); // all chefs"
+router.get('/dishes/dishId', chefController); // dish by id
+router.get('/dishes/filter', chefController); // filter dishes
+module.exports = router;
