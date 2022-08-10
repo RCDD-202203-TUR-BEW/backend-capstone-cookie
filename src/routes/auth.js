aa0381b98985d7ee983c1f3c6f848604d44b0bee
Hala Alkhellow <halakhellow1996@gmail.com>
2022-08-10 03:27:02 +0300

    refactor: move auth routes to be in a separate file in the routes folder

---
+++ src/routes/auth.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const authControllers = require('../controllers/auth');
+const {
+  usernameValidator,
+  emailValidator,
+  passwordValidator,
+  phoneValidator,
+  handleValidation,
+} = require('../middleware/validation');
+
+const router = express.Router();
+
+router.post(
+  '/signup',
+  usernameValidator,
+  emailValidator,
+  passwordValidator,
+  phoneValidator,
+  handleValidation,
+  authControllers.signup
+);
+
+router.post('/signin', authControllers.signin);
+router.get('/signout', authControllers.signout);
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-
+const authRoutes = require('./auth');
 const customerRoutes = require('./customer');
 const chefRoutes = require('./chef');
 const adminRoutes = require('./admin');
 
-router.get('/customer', customerRoutes); //    /api/customer
-router.get('/chef', chefRoutes); //    /api/chef
-router.get('/admin', adminRoutes); //    /api/admin
-
+router.use('/auth', authRoutes);
+// router.use('/customer', customerRoutes); //    /api/customer
+// router.use('/chef', chefRoutes); //    /api/chef
+router.use('/admin', adminRoutes); //    /api/admin
 
 module.exports = router;
-
