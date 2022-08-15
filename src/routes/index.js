c54327736151468f17415ddcb0b01bf9f14acf98
Kishi <66186149+Kishimoto96@users.noreply.github.com>
2022-08-15 23:51:19 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-// const express = require('express');
+const express = require('express');
 
-// const router = express.Router();
+const router = express.Router();
 
 
 const authRoutes = require('./auth');
