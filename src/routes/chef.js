24ea07671a0cc827284e7d695770f8ac34a07d48
Hala Alkhellow <halakhellow1996@gmail.com>
2022-08-19 03:19:11 +0300

    protect chef related routes with the middleware

--- src/routes/chef.js
+++ src/routes/chef.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const chefControllers = require('../controllers/chef');
 const isAuthenticated = require('../middleware/isAuthenticated');
+const permit = require('../middleware/authorization');
 
 // IMPORTANT NOTE: The order of the routes matters as middleware functions are executed sequentially
 
@@ -13,32 +14,59 @@ router.get('/nearby-chefs', chefControllers.getNearbyChefs);
 router.get('/:username', chefControllers.getSpecificChef);
 
 // PRIVATE ROUTES
-router.get('/profile/:username', isAuthenticated, chefControllers.seeProfile);
+router.get(
+  '/profile/:username',
+  isAuthenticated,
+  permit('chef'),
+  chefControllers.seeProfile
+);
 router.put(
   '/profile/:username',
+  permit('chef'),
   isAuthenticated,
   chefControllers.updateProfile
 );
-router.post('/:username/profile', chefControllers.addLocation);
-router.put('/:username/profile/:locationId', chefControllers.updateLocation);
-router.delete('/:username/profile/:locationId', chefControllers.deleteLocation);
+router.post('/:username/profile', permit('chef'), chefControllers.addLocation);
+router.put(
+  '/:username/profile/:locationId',
+  permit('chef'),
+  chefControllers.updateLocation
+);
+router.delete(
+  '/:username/profile/:locationId',
+  permit('chef'),
+  chefControllers.deleteLocation
+);
 
-router.post('/dishes/:username', isAuthenticated, chefControllers.addDish);
+router.post(
+  '/dishes/:username',
+  isAuthenticated,
+  permit('chef'),
+  chefControllers.addDish
+);
 router.put(
   '/dishes/:username/:dishId',
   isAuthenticated,
+  permit('chef'),
   chefControllers.updateDishInfos
 );
 router.delete(
   '/dishes/:username/:dishId',
   isAuthenticated,
+  permit('chef'),
   chefControllers.deleteDish
 );
 
-router.get('/orders/:username', isAuthenticated, chefControllers.getOrders);
+router.get(
+  '/orders/:username',
+  isAuthenticated,
+  permit('chef'),
+  chefControllers.getOrders
+);
 router.put(
   '/orders/:username/:orderId',
   isAuthenticated,
+  permit('chef'),
   chefControllers.finishPreparation
 );
 
