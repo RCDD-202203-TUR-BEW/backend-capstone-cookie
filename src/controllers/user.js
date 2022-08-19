2b4b7bfca84fe6faf29c1276629377bbc9cffe7f
Hala Alkhellow <halakhellow1996@gmail.com>
2022-08-20 00:13:57 +0300

    add location controllers and routes to the shared files between customers and chefs

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -54,8 +54,164 @@ const uploadUserAvatar = async (req, res) => {
   }
 };
 
+// Location controllers
+const createLocation = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const newLocation = req.body;
+    const { _id } = req.user;
+
+    if (_id !== userId)
+      res
+        .status(401)
+        .json({ message: "You're not authorized to view this page" });
+    else {
+      const user = await UserModel.findById(userId);
+
+      user.locations.push(newLocation);
+      user.save();
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getAllLocations = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { _id } = req.user;
+    if (_id !== userId)
+      res
+        .status(401)
+        .json({ message: "You're not authorized to view this page" });
+    else {
+      const user = await UserModel.findById(userId);
+
+      // returns all locations as an array of objects
+
+      res.status(200).json(user.locations);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getSpecificLocation = async (req, res) => {
+  try {
+    const { userId, locationId } = req.params;
+    const { _id } = req.user;
+    if (_id !== userId)
+      res
+        .status(401)
+        .json({ message: "You're not authorized to view this page" });
+    else {
+      const user = await UserModel.findById(userId);
+
+      // get the index of the location with the given id
+      /* eslint no-underscore-dangle: 0 */
+      const locationIndex = user.locations.findIndex(
+        (location) => location._id.toString() === locationId.toString()
+      );
+
+      const location = user.locations[locationIndex];
+      if (location) res.json(location);
+      else res.status(404).json({ message: 'invalid location id' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateLocation = async (req, res) => {
+  try {
+    const { userId, locationId } = req.params;
+
+    const { _id } = req.user;
+    if (_id !== userId)
+      res
+        .status(401)
+        .json({ message: "You're not authorized to view this page" });
+    else {
+      const user = await UserModel.findById(userId);
+
+      // get the index of the location with the given id
+      /* eslint no-underscore-dangle: 0 */
+      const locationIndex = user.locations.findIndex(
+        (location) => location._id.toString() === locationId.toString()
+      );
+
+      if (locationIndex === -1)
+        res.status(404).json({ message: 'invalid location id' });
+      else {
+        // open an empty set for holding new values from req
+        const updateSet = {};
+
+        // assign each new value to the empty set
+        const obj = user.locations[locationIndex].toObject();
+
+        Object.keys(obj).forEach((key) => {
+          if (!req.body[key]) {
+            updateSet[`${key}`] = user.locations[locationIndex][`${key}`];
+          } else {
+            updateSet[`${key}`] = req.body[key];
+          }
+        });
+
+        // update the location object reached by the index
+        user.locations[locationIndex] = updateSet;
+        user.save();
+
+        res.status(200).json(user);
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteLocation = async (req, res) => {
+  try {
+    const { userId, locationId } = req.params;
+    const { _id } = req.user;
+    if (_id !== userId)
+      res
+        .status(401)
+        .json({ message: "You're not authorized to view this page" });
+    else {
+      const user = await UserModel.findById(userId);
+
+      // get the index of the location with the given id
+      /* eslint no-underscore-dangle: 0 */
+
+      const locationIndex = user.locations.findIndex(
+        (location) => location._id.toString() === locationId.toString()
+      );
+      if (locationIndex === -1)
+        res.status(404).json({ message: 'invalid location id' });
+      else {
+        // delete the location object reached by the index
+        user.locations.splice(locationIndex, 1);
+
+        user.save();
+
+        // return current locations
+        const { locations } = user;
+        res.status(200).json(locations);
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   fetchUserAvatar,
   deleteUserAvatar,
   uploadUserAvatar,
+  createLocation,
+  getSpecificLocation,
+  getAllLocations,
+  updateLocation,
+  deleteLocation,
 };
