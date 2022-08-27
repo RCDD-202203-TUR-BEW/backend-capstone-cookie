7eb2abe81fe2dd57afc512ebd5e0ac2d95ecf482
Hamza Aymen <hamza.ayment@gmail.com>
2022-08-28 01:12:30 +0300

    merge middleware and nodemailer

--- src/middleware/isVerified.js
+++ src/middleware/isVerified.js
@@ -1,5 +1,9 @@
-const isVerified = (req, res, next) => {
-  if (req.user.email_verified) return next();
+const customerModel = require('../models/user').User;
+
+const isVerified = async (req, res, next) => {
+  const user = await customerModel.findById(req.user.id);
+
+  if (user.email_verified) return next();
   return res.status(403).json({ message: 'Your email have not verified yet' });
 };
 
